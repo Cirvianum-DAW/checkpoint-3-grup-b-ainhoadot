fb092ca2fd2744144c6e2a245e861d2f77aa8aa4
John Doe <jordi.forcada@cirvianum.cat>
2024-04-30 16:18:59 +0200

    2nd commit
    Seguim amb les validacions.

--- UF3/validacions.js
+++ UF3/validacions.js
@@ -5,10 +5,8 @@ const nameInput = document.querySelector('input[name="name"]');
 const surnameInput = document.querySelector('input[name="surname"]');
 const emailInput = document.querySelector('input[name="email"]');
 const ageInput = document.querySelector('input[name="age"]');
-const genderInput = document.querySelector('select[name="gender"]');
-const messageInput = document.querySelector('select[textarea="message"]');
-
-
+// const genderInput = document.querySelector('select[name="gender"]');
+// const messageInput = document.querySelector('select[textarea="message"]');
 
 // Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
 // els errors allà on consideris lògic
@@ -32,12 +30,51 @@ function displayError(input, message) {
 
 // Cada validació...
 
+// Funció per validar el nom
 function validateName() {
   removeExistingError(nameInput);
-  // Validació del nom...
+  const nameValue = nameInput.value; 
+  const nameRegex = /[0-9]/; 
+  
+  if (nameValue.length < 3 || nameValue.length >= 50 || nameRegex.test(nameValue)) {
+    displayError(nameInput, "El nom no és vàlid");
+  }
+}
 
-  // Si no és vàlid...
-  displayError(nameInput, "El nom no és vàlid");
+// Funció per validar el cognom
+function validateSurname() {
+  removeExistingError(surnameInput);
+  const surnameValue = surnameInput.value; 
+  const surnameRegex = /[0-9]/;
+  
+  if (surnameValue.length < 3 || surnameValue.length >= 50 || surnameRegex.test(surnameValue)) {
+    displayError(surnameInput, "El cognom no és vàlid");
+  }
+}
+
+// Funció per validar el email
+function validateEmail() {
+  removeExistingError(emailInput);
+  const emailValue = emailInput.value; 
+  const emailRegex = /^[^@]+@[^@]+\.[^@]+$/;
+  
+  if (!emailRegex.test(emailValue)) {
+    displayError(emailInput, "El email no és vàlid");
+  }
+}
+
+// Funció per validar que es major d'edat
+function validateAge() {
+  removeExistingError(ageInput);
+  const ageValue = ageInput.value;
+
+  if (ageValue < 18){
+
+  }
 }
 
 // addEventListeners...
+nameInput.addEventListener('blur', validateName);
+surnameInput.addEventListener('blur', validateSurname);
+emailInput.addEventListener('blur', validateEmail);
+ageInput.addEventListener('blur', validateAge);
